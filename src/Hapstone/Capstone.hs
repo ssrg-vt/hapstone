eaf39e5c74172ddafb9e71007baf294a2e6b084c
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-03-31 21:42:46 +0200

    Added proper high-level bindings

--- src/Hapstone/Capstone.hs
+++ src/Hapstone/Capstone.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE RecordWildCards #-}
-module Hapstone.Capstone where
+module Hapstone.Capstone 
+    ( disasmIO
+    , disasmSimpleIO
+    , Disassembler(..)
+    , defaultSkipdataStruct
+    , defaultAction
+    ) where
 
 import Data.Word
 
@@ -7,22 +13,47 @@ import Foreign.Ptr
 
 import Hapstone.Internal.Capstone
 
-import System.IO.Unsafe (unsafePerformIO)
-
--- default setup for skipdata
+-- | default setup for skipdata
 defaultSkipdataStruct :: CsSkipdataStruct
 defaultSkipdataStruct = CsSkipdataStruct ".db" nullFunPtr nullPtr
 
+-- | default action to run on each instruction
+defaultAction :: Csh -> CsInsn -> IO ()
+defaultAction _ _ = return ()
+
+-- | a structure holding settings for a disassembling action
 data Disassembler a = Disassembler
-    { arch :: CsArch
-    , modes :: [CsMode]
-    , buffer :: [Word8]
-    , addr :: Word64
-    , num :: Int
-    , detail :: Bool
-    , skip :: Maybe CsSkipdataStruct
-    , action :: Csh -> CsInsn -> IO a
+    { arch :: CsArch -- ^ architecture
+    , modes :: [CsMode] -- ^ disassembling modes
+    , buffer :: [Word8] -- ^ buffer to disassemble
+    , addr :: Word64 -- ^ address of first byte in the buffer
+    , num :: Int -- ^ number of instructions to disassemble (0 for maximum)
+    , detail :: Bool -- ^ include detailed information?
+    , skip :: Maybe CsSkipdataStruct -- ^ setup SKIPDATA options
+    , action :: Csh -> CsInsn -> IO a -- ^ action to run on each instruction
     }
 
-disasmIO :: Disassembler a -> IO (Either CsErr [CsInsn])
-disasmIO Disassembler{..} = undefined
+-- | run a disassembler, throwing away the results of the custom action
+disasmSimpleIO :: Disassembler a -> IO (Either CsErr [CsInsn])
+disasmSimpleIO = fmap (fmap (map fst)) . disasmIO
+
+-- | run a Disassembler
+disasmIO :: Disassembler a -> IO (Either CsErr [(CsInsn, a)])
+disasmIO d@Disassembler{..} = do (err, handle) <- csOpen arch modes
+                                 case err of
+                                   CsErrOk -> disasmIOWithHandle handle
+                                   _ -> return $ Left err
+    where disasmIOWithHandle handle = do
+              err <- if detail
+                        then csOption handle CsOptDetail CsOptOn
+                        else return CsErrOk
+              case err of
+                CsErrOk -> disasmIOWithHandleDetail handle
+                _ -> return $ Left err
+          disasmIOWithHandleDetail handle = do
+              err <- csSetSkipdata handle skip
+              case err of
+                CsErrOk -> do insns <- csDisasm arch handle buffer addr num
+                              as <- mapM (action handle) insns
+                              return . Right $ zip insns as
+                _ -> return $ Left err
