f77d1b7ac8ebd0ef34a4dbc4a8be703c142176af
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-04-03 21:51:18 +0200

    SPARC tests finished

--- test/Internal/Sparc/StorableSpec.hs
+++ test/Internal/Sparc/StorableSpec.hs
@@ -17,6 +17,17 @@ spec = describe "Hapstone.Internal.Sparc" $ do
     csSparcOpSpec
     csSparcSpec
 
+getSparcOpMemStruct :: IO SparcOpMemStruct
+getSparcOpMemStruct = do
+    ptr <- mallocArray (sizeOf sparcOpMemStruct) :: IO (Ptr Word8)
+    poke ptr 0x1
+    poke (plusPtr ptr 1) (0x23 :: Word8)
+    poke (plusPtr ptr 4) (0x01234567 :: Word32)
+    peek (castPtr ptr) <* free ptr
+
+sparcOpMemStruct :: SparcOpMemStruct
+sparcOpMemStruct = SparcOpMemStruct 0x1 0x23 0x01234567
+
 -- | SparcOpMemStruct spec
 sparcOpMemStructSpec :: Spec
 sparcOpMemStructSpec = describe "Storable SparcOpMemStruct" $ do
@@ -25,7 +36,17 @@ sparcOpMemStructSpec = describe "Storable SparcOpMemStruct" $ do
     it "has matching peek- and poke-implementations" $ property $
         \s@SparcOpMemStruct{} ->
             alloca (\p -> poke p s >> peek p) `shouldReturn` s
-    it "parses correctly" $ pendingWith "use a binary string generated"
+    it "parses correctly" $ getSparcOpMemStruct `shouldReturn` sparcOpMemStruct
+
+getCsSparcOp :: IO CsSparcOp
+getCsSparcOp = do
+    ptr <- mallocArray (sizeOf csSparcOp) :: IO (Ptr Word8)
+    poke (castPtr ptr) (fromIntegral $ fromEnum SparcOpImm :: Int32)
+    poke (plusPtr ptr 4) (234 :: Int32)
+    peek (castPtr ptr) <* free ptr
+
+csSparcOp :: CsSparcOp
+csSparcOp = Imm 234
 
 -- | CsSparcipsOp spec
 csSparcOpSpec :: Spec
@@ -35,7 +56,19 @@ csSparcOpSpec = describe "Storable CsSparcOp" $ do
     it "has matching peek- and poke-implementations" $ property $
         \s ->
             alloca (\p -> poke p s >> (peek p :: IO CsSparcOp)) `shouldReturn` s
-    it "parses correctly" $ pendingWith "use a binary string generated"
+    it "parses correctly" $ getCsSparcOp `shouldReturn` csSparcOp
+
+getCsSparc :: IO CsSparc
+getCsSparc = do
+    ptr <- mallocArray (sizeOf csSparc) :: IO (Ptr Word8)
+    poke (castPtr ptr) (fromIntegral $ fromEnum SparcCcIccL :: Int32)
+    poke (plusPtr ptr 4) (fromIntegral $ fromEnum SparcHintA :: Int32)
+    poke (plusPtr ptr 8) (1 :: Word8)
+    poke (plusPtr ptr 12) csSparcOp
+    peek (castPtr ptr) <* free ptr
+
+csSparc :: CsSparc
+csSparc = CsSparc SparcCcIccL SparcHintA [csSparcOp]
 
 -- | CsSparc spec
 csSparcSpec :: Spec
@@ -45,4 +78,4 @@ csSparcSpec = describe "Storable CsSparc" $ do
     it "has matching peek- and poke-implementations" $ property $
         \s@CsSparc{} ->
             alloca (\p -> poke p s >> peek p) `shouldReturn` s
-    it "parses correctly" $ pendingWith "use a binary string generated"
+    it "parses correctly" $ getCsSparc `shouldReturn` csSparc
