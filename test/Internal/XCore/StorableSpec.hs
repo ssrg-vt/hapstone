7acfb9a0ec5e10fb5bcbf894b809ec609f8595d3
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-04-03 23:14:50 +0200

    XCore tests finished

--- test/Internal/XCore/StorableSpec.hs
+++ test/Internal/XCore/StorableSpec.hs
@@ -17,6 +17,18 @@ spec = describe "Hapstone.Internal.XCore" $ do
     csXCoreOpSpec
     csXCoreSpec
 
+getXCoreOpMemStruct :: IO XCoreOpMemStruct
+getXCoreOpMemStruct = do
+    ptr <- mallocArray (sizeOf xCoreOpMemStruct) :: IO (Ptr Word8)
+    poke ptr 0x1
+    poke (plusPtr ptr 1) (0x2 :: Word8)
+    poke (plusPtr ptr 4) (0x01234567 :: Int32)
+    poke (plusPtr ptr 8) (1 :: Int32)
+    peek (castPtr ptr) <* free ptr
+
+xCoreOpMemStruct :: XCoreOpMemStruct
+xCoreOpMemStruct = XCoreOpMemStruct 0x1 0x2 0x01234567 1
+
 -- | XCoreOpMemStruct spec
 xCoreOpMemStructSpec :: Spec
 xCoreOpMemStructSpec = describe "Storable XCoreOpMemStruct" $ do
@@ -25,7 +37,17 @@ xCoreOpMemStructSpec = describe "Storable XCoreOpMemStruct" $ do
     it "has matching peek- and poke-implementations" $ property $
         \s@XCoreOpMemStruct{} ->
             alloca (\p -> poke p s >> peek p) `shouldReturn` s
-    it "parses correctly" $ pendingWith "use a binary string generated"
+    it "parses correctly" $ getXCoreOpMemStruct `shouldReturn` xCoreOpMemStruct
+
+getCsXCoreOp :: IO CsXCoreOp
+getCsXCoreOp = do
+    ptr <- mallocArray (sizeOf csXCoreOp) :: IO (Ptr Word8)
+    poke (castPtr ptr) (fromIntegral $ fromEnum XcoreOpImm :: Int32)
+    poke (plusPtr ptr 4) (0x01234567 :: Int32)
+    peek (castPtr ptr) <* free ptr
+
+csXCoreOp :: CsXCoreOp
+csXCoreOp = Imm 0x01234567
 
 -- | CsXCoreipsOp spec
 csXCoreOpSpec :: Spec
@@ -35,7 +57,17 @@ csXCoreOpSpec = describe "Storable CsXCoreOp" $ do
     it "has matching peek- and poke-implementations" $ property $
         \s -> alloca (\p -> poke p s >> (peek p :: IO CsXCoreOp))
         `shouldReturn` s
-    it "parses correctly" $ pendingWith "use a binary string generated"
+    it "parses correctly" $ getCsXCoreOp `shouldReturn` csXCoreOp
+
+getCsXCore :: IO CsXCore
+getCsXCore = do
+    ptr <- mallocArray (sizeOf csXCore) :: IO (Ptr Word8)
+    poke ptr 1
+    poke (plusPtr ptr 4) csXCoreOp
+    peek (castPtr ptr) <* free ptr
+
+csXCore :: CsXCore
+csXCore = CsXCore [csXCoreOp]
 
 -- | CsXCore spec
 csXCoreSpec :: Spec
@@ -45,4 +77,4 @@ csXCoreSpec = describe "Storable CsXCore" $ do
     it "has matching peek- and poke-implementations" $ property $
         \s@CsXCore{} ->
             alloca (\p -> poke p s >> peek p) `shouldReturn` s
-    it "parses correctly" $ pendingWith "use a binary string generated"
+    it "parses correctly" $ getCsXCore `shouldReturn` csXCore
